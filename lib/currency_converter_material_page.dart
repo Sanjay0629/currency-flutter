4626636f7d7201fb943f39c90dfeef3959e62148
Sanjay0629 <sanjaysivaprakash86@gmail.com>
2025-02-19 22:46:01 +0530

    Updated code part1

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -2,21 +2,61 @@ import 'package:flutter/material.dart';
 
 class CurrencyConverterMaterialpage extends StatelessWidget {
   const CurrencyConverterMaterialpage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.green,
+    final border = OutlineInputBorder(
+      borderSide: BorderSide(
+        width: 2.0,
+        color: Colors.black,
+        style: BorderStyle.solid,
+      ),
+      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               '0',
               style: TextStyle(
-                fontSize: 45,
+                fontSize: 55,
                 fontWeight: FontWeight.bold,
                 color: Colors.white,
               ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              //margin: EdgeInsets.all(5),
+              child: TextField(
+                style: const TextStyle(color: Colors.black),
+                decoration: InputDecoration(
+                  hintText: 'Please Enter The Amount in USD',
+                  hintStyle: TextStyle(color: Colors.black),
+                  prefixIcon: const Icon(Icons.monetization_on_outlined),
+                  prefixIconColor: Colors.black,
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: border,
+                  enabledBorder: border,
+                ),
+              ),
+            ),
+            //buttons
+            //raised
+            //appears like a text
+            TextButton(
+              onPressed: () {
+                debugPrint('Button clicked');
+              },
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(Colors.black),
+                foregroundColor: WidgetStatePropertyAll(Colors.white),
+              ),
+              child: const Text('Convert'),
             ),
           ],
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,6 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: CurrencyConverterMaterialpage(),
-    );
+    return const MaterialApp(home: CurrencyConverterMaterialpage());
   }
 }
